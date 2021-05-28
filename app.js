b350d159eb6bf80d35acc8954b3e91988089e3bd
smdsaran <saranthuppaki056@gmail.com>
2021-05-28 13:54:28 +0530

    Added bcrypt with saltRound to do hash

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 //console.log(md5('1234'));
 const app = express();
@@ -38,46 +39,57 @@ app.get("/register" , function(req , res) {
   res.render("register");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
-});
+
 
 app.post("/register" , function(req , res) {
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
-  const newUser = new User({
-    email:  username, 
-  password: password
-  });
+  bcrypt.hash(password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email:  username, 
+    password: hash
+    });
+  
+    newUser.save(function(err) {
+      if(!err) {
+        res.render("secrets");
+      } else {
+        res.send(err);
+      }
+    });
+});
 
-  newUser.save(function(err) {
-    if(!err) {
-      res.render("secrets");
-    } else {
-      res.send(err);
-    }
-  });
+  
 });
 
 app.post("/login" , function(req , res) {
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   User.findOne({email: username} , function(err , resultDoc) {
     if(resultDoc) {
-      if(resultDoc.password === password) {
-        res.render("secrets");
+      bcrypt.compare(password, resultDoc.password , function(err, result) {
+        if(result == true)  {
+          res.render("secrets");
+        }
+
+        else {
+          res.send("Username  and password mismatch .")
+        }
+      });
       }
 
-      else {
-        res.send("Username  and password mismatch .")
-      }
-    }
+      
+    
 
     else {
       res.send(err);
     }
 
   });
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
 });
